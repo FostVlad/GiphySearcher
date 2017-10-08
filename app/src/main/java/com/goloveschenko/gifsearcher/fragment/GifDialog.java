f5207f6838e54702b473473c5841765d77ccf7e6
Vlad Goloveschenko <vladislavan23@gmail.com>
2017-10-08 20:14:14 +0300

    added progress bar for big size gif

--- app/src/main/java/com/goloveschenko/gifsearcher/fragment/GifDialog.java
+++ app/src/main/java/com/goloveschenko/gifsearcher/fragment/GifDialog.java
@@ -3,13 +3,19 @@ package com.goloveschenko.gifsearcher.fragment;
 import android.app.Dialog;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
-import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.load.resource.gif.GifDrawable;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
 import com.goloveschenko.gifsearcher.R;
 
 public class GifDialog extends DialogFragment {
@@ -26,15 +32,28 @@ public class GifDialog extends DialogFragment {
     @NonNull
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
+        Dialog dialog = super.onCreateDialog(savedInstanceState);
         View view = LayoutInflater.from(getActivity()).inflate(R.layout.dialog_gif, null);
+        dialog.setContentView(view);
         ImageView imageView = (ImageView) view.findViewById(R.id.dialog_gif_image);
+        ProgressBar progressBar = (ProgressBar) view.findViewById(R.id.dialog_gif_progress);
         Glide.with(getActivity())
                 .asGif()
                 .load(getArguments().getString(ARG_KEY))
-                .into(imageView);
+                .listener(new RequestListener<GifDrawable>() {
+                    @Override
+                    public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<GifDrawable> target, boolean isFirstResource) {
+                        progressBar.setVisibility(View.GONE);
+                        return false;
+                    }
 
-        return new AlertDialog.Builder(getActivity())
-                .setView(view)
-                .create();
+                    @Override
+                    public boolean onResourceReady(GifDrawable resource, Object model, Target<GifDrawable> target, DataSource dataSource, boolean isFirstResource) {
+                        progressBar.setVisibility(View.GONE);
+                        return false;
+                    }
+                })
+                .into(imageView);
+        return dialog;
     }
 }
